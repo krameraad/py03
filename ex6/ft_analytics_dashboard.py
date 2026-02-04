bffa945249c8f9864a7da82298db7086114d8d9a
ekramer <ekramer@student.42.fr>
2026-02-04 19:11:53 +0100

    ex6 done

--- ex6/ft_analytics_dashboard.py
+++ ex6/ft_analytics_dashboard.py
@@ -1,54 +1,39 @@
 #!/usr/bin/env python3
 
 X = "\033[0m"
-D = "\033[2m"
 H = "\033[1m"
 HC = "\033[1;96m"
-HY = "\033[1;93m"
-XH = "\033[0;1m"
 
-data = {
+data: dict[str, dict | list[str]] = {
     "players": {
         "alice": {
             "level": 41,
             "total_score": 2824,
-            "sessions_played": 13,
-            "favorite_mode": "ranked",
             "achievements_count": 5
         },
         "bob": {
             "level": 16,
             "total_score": 4657,
-            "sessions_played": 27,
-            "favorite_mode": "ranked",
             "achievements_count": 2
         },
         "charlie": {
             "level": 44,
             "total_score": 9935,
-            "sessions_played": 21,
-            "favorite_mode": "ranked",
             "achievements_count": 7
         },
         "diana": {
             "level": 3,
             "total_score": 1488,
-            "sessions_played": 21,
-            "favorite_mode": "casual",
             "achievements_count": 4
         },
         "eve": {
             "level": 33,
             "total_score": 1434,
-            "sessions_played": 81,
-            "favorite_mode": "casual",
             "achievements_count": 7
         },
         "frank": {
             "level": 15,
             "total_score": 359,
-            "sessions_played": 85,
-            "favorite_mode": "competitive",
             "achievements_count": 1
         }
     },
@@ -74,56 +59,51 @@ data = {
 
 print(f"{H}\n=== Game Analytics Dashboard ==={X}")
 
-
 print(f"{H}\n=== List Comprehension Examples ==={X}")
 
-players = data["players"]
-
-high_scorers = [player for player in players
-                if players[player]["total_score"] > 2000]
+high_scorers = [k for k, v in data["players"].items()
+                if v["total_score"] > 2000]
 print(f"{HC}High scorers (>2000){X}\t: {high_scorers}")
 
-scores_doubled = [player["total_score"] * 2
-                  for player in players.values()]
+scores_doubled = [v["total_score"] * 2
+                  for v in data["players"].values()]
 print(f"{HC}Scores doubled{X}\t\t: {scores_doubled}")
 
-active_players = [player for player in players]
+active_players = [k for k in data["players"]]
 print(f"{HC}Active players{X}\t\t: {active_players}")
 
 
 print(f"{H}\n=== Dict Comprehension Examples ==={X}")
 
-player_scores = {player: players[player]["total_score"]
-                 for player in players}
+player_scores = {k: v["total_score"]
+                 for k, v in data["players"].items()}
 print(f"{HC}Player scores{X}\t\t: {player_scores}")
 
-score_categories = {"high": 0, "medium": 0, "low": 0}
-score_categories["high"] = {"high": score_categories["high"] + 1
-                            for player in data["players"].values()
-                            if player["total_score"] > 3000}
-score_categories["medium"] = {"medium": score_categories["medium"] + 1
-                              for player in data["players"].values()
-                              if 1000 < player["total_score"] <= 3000}
-score_categories["low"] = {"low": score_categories["low"] + 1
-                           for player in data["players"].values()
-                           if player["total_score"] <= 1000}
+score_categories = {
+    "high": len([k for k, v in data["players"].items()
+                 if v["total_score"] > 2000]),
+    "medium": len([k for k, v in data["players"].items()
+                   if v["total_score"] > 1000]),
+    "low": len([k for k, v in data["players"].items()
+                if v["total_score"] <= 1000])
+}
 print(f"{HC}Score categories{X}\t: {score_categories}")
 
-achievement_counts = {player: players[player]["achievements_count"]
-                      for player in players}
+achievement_counts = {k: v["achievements_count"]
+                      for k, v in data["players"].items()}
 print(f"{HC}Achievement counts{X}\t: {achievement_counts}")
 
 
 print(f"{H}\n=== Set Comprehension Examples ==={X}")
 
-unique_players = {player for player in players}
+unique_players = {k for k in data["players"]}
 print(f"{HC}Unique players{X}\t\t: {unique_players}")
 
-unique_achievements = {achievement for achievement in data["achievements"]}
+unique_achievements = {k for k in data["achievements"]}
 print(f"{HC}Unique achievements{X}\t: {unique_achievements}")
 
-active_regions = {region for region in data["regions"]
-                  if "w" not in region}
+active_regions = {k for k in data["regions"]
+                  if "w" not in k}
 print(f"{HC}Active regions{X}\t\t: {active_regions}")
 
 
@@ -134,7 +114,7 @@ print(f"{HC}Average score{X}\t\t\t: "
       f"{sum(scores_doubled) / len(scores_doubled) / 2:.1f}")
 
 top_performer = ("", -1)
-for player in players:
-    if players[player]["total_score"] > top_performer[1]:
-        top_performer = (player, players[player]["total_score"])
+for k, v in data["players"].items():
+    if v["total_score"] > top_performer[1]:
+        top_performer = (k, v["total_score"])
 print(f"{HC}Top performer{X}\t\t\t: {top_performer[0]}")
